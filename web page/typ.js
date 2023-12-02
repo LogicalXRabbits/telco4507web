28da5dc2ffacf3d29e68c05ca4edeac2667e2dd7
bolulembang <mpluto772@gmail.com>
2023-12-03 02:49:53 +0700

    updateeee

--- web page/typ.js
+++ web page/typ.js
@@ -1,36 +1,16 @@
-// Wait for the DOM to be ready
-document.addEventListener('DOMContentLoaded', function () {
-  // Get all the navigation items
-  const navItems = document.querySelectorAll('.item');
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll("nav a");
 
-  // Get the underline element
-  const underline = document.querySelector('.underline');
+  navLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
 
-  // Function to update the width of the underline based on the active nav item
-  function updateUnderlineWidth() {
-    // Get the width of the active nav item
-    const activeItemWidth = document.querySelector('.item.active').offsetWidth;
+      const targetId = link.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
 
-    // Set the width of the underline to match the active nav item's width
-    underline.style.width = `${activeItemWidth}px`;
-  }
-
-  // Add click event listeners to each nav item
-  navItems.forEach(function (item) {
-    item.addEventListener('click', function () {
-      // Remove the 'active' class from all nav items
-      navItems.forEach(function (navItem) {
-        navItem.classList.remove('active');
-      });
-
-      // Add the 'active' class to the clicked nav item
-      item.classList.add('active');
-
-      // Update the width of the underline
-      updateUnderlineWidth();
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: "smooth" });
+      }
     });
   });
-
-  // Update the width of the underline initially
-  updateUnderlineWidth();
 });
