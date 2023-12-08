a79171e752ffb071ddd2845c9df207392c23bde1
bolulembang <mpluto772@gmail.com>
2023-12-08 15:37:04 +0700

    fix brand no read

--- script/typ.js
+++ script/typ.js
@@ -1,53 +1,72 @@
 document.addEventListener("DOMContentLoaded", function () {
   const navLinks = document.querySelectorAll("nav a");
- 
+
   navLinks.forEach((link) => {
-     link.addEventListener("click", (e) => {
-       e.preventDefault();
- 
-       const targetId = link.getAttribute("href").substring(1);
-       const targetElement = document.getElementById(targetId);
- 
-       if (targetElement) {
-         targetElement.scrollIntoView({ behavior: "smooth" });
-       }
-     });
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      const targetId = link.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: "smooth" });
+      }
+    });
   });
- 
+
   // Your custom function to detect the user's location and device information
   async function getUserInformation() {
-     const response = await fetch(
-       "https://api.ipregistry.co/?key=zqx0w4uhrljxqldg"
-     );
-     const data = await response.json();
- 
-     return {
-       city: data.location.city,
-       device: data.devices.brand,
-       ip: data.ip,
-       time: new Date().toLocaleString(),
-     };
+    try {
+      const response = await fetch(
+        "https://api.ipregistry.co/?key=YOUR_API_KEY"
+      );
+      const data = await response.json();
+
+      // Check if 'devices' object exists
+      if (data.devices) {
+        return {
+          city: data.location.city,
+          device: data.devices.brand,
+          ip: data.ip,
+          time: new Date().toLocaleString(),
+        };
+      } else {
+        throw new Error("Cannot read 'devices' object");
+      }
+    } catch (error) {
+      console.error("Failed to fetch user information:", error);
+      throw error; // rethrow the error to handle it later if needed
+    }
   }
- 
+
   // Function to send data to Discord via webhook
   async function sendToDiscord(data) {
-     const url =
-       "https://discord.com/api/webhooks/1182580231261921290/GQNh98TtfoIS16xTY-6MlhtMBGC7ubRyvqX_qP8_arP6Mr9u74xWwpD6zV_8f01IvbnV";
-     const message = `Ada seorang user mengunjungi website anda:\n\n- City: ${data.city}\n- Device: ${data.device}\n- IP: ${data.ip}\n- Time: ${data.time}`;
- 
-     const response = await fetch(url, {
-       method: "POST",
-       headers: {
-         "Content-Type": "application/json",
-       },
-       body: JSON.stringify({ content: message }),
-     });
- 
-     return response.ok;
+    try {
+      const url =
+        "https://discord.com/api/webhooks/1182580231261921290/GQNh98TtfoIS16xTY-6MlhtMBGC7ubRyvqX_qP8_arP6Mr9u74xWwpD6zV_8f01IvbnV";
+      const message = `Ada seorang user mengunjungi website anda:\n\n- City: ${data.city}\n- Device: ${data.device}\n- IP: ${data.ip}\n- Time: ${data.time}`;
+
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ content: message }),
+      });
+
+      if (!response.ok) {
+        console.error("Failed to send data to Discord:", response.statusText);
+      }
+
+      return response.ok;
+    } catch (error) {
+      console.error("Error in sending data to Discord:", error);
+      throw error; // rethrow the error to handle it later if needed
+    }
   }
- 
+
   // Call the function to get user information and send it to Discord
-  getUserInformation().then((data) => {
-     sendToDiscord(data);
-  });
- });
+  getUserInformation()
+    .then((data) => sendToDiscord(data))
+    .catch((error) => console.error("Failed to get and send user information:", error));
+});
