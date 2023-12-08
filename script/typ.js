f454ce32149cb7307f21558552b01b25ea777a36
bolulembang <mpluto772@gmail.com>
2023-12-08 14:29:30 +0700

    modify embed message discord log

--- script/typ.js
+++ script/typ.js
@@ -29,22 +29,51 @@ document.addEventListener("DOMContentLoaded", function () {
     };
   }
 
-  // Function to send data to Discord via webhook
-  async function sendToDiscord(data) {
-    const url =
-      "https://discord.com/api/webhooks/1182580231261921290/GQNh98TtfoIS16xTY-6MlhtMBGC7ubRyvqX_qP8_arP6Mr9u74xWwpD6zV_8f01IvbnV";
-    const message = `New user visit detected:\n\n- City: ${data.city}\n- Device: ${data.device}\n- IP: ${data.ip}\n- Time: ${data.time}`;
-
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ content: message }),
-    });
-
-    return response.ok;
-  }
+// Function to send data to Discord via webhook
+async function sendToDiscord(data) {
+  const url = "https://discord.com/api/webhooks/1182580231261921290/GQNh98TtfoIS16xTY-6MlhtMBGC7ubRyvqX_qP8_arP6Mr9u74xWwpD6zV_8f01IvbnV";
+ 
+  const message = {
+     content: `New user visit detected:`,
+     embeds: [
+       {
+         color: 3447003,
+         fields: [
+           {
+             name: "City",
+             value: data.city,
+             inline: true,
+           },
+           {
+             name: "Device",
+             value: data.device,
+             inline: true,
+           },
+           {
+             name: "IP",
+             value: data.ip,
+             inline: true,
+           },
+           {
+             name: "Time",
+             value: data.time,
+             inline: true,
+           },
+         ],
+       },
+     ],
+  };
+ 
+  const response = await fetch(url, {
+     method: "POST",
+     headers: {
+       "Content-Type": "application/json",
+     },
+     body: JSON.stringify(message),
+  });
+ 
+  return response.ok;
+ }
 
   // Call the function to get user information and send it to Discord
   getUserInformation().then((data) => {
