a455f4bdba500313a4d1cf181dab6c3c77739efb
bolulembang <mpluto772@gmail.com>
2023-12-08 16:25:55 +0700

    fix notify lagi

--- script/typ.js
+++ script/typ.js
@@ -85,12 +85,12 @@ getUserInformation()
 
 document.addEventListener("contextmenu", function (e) {
   e.preventDefault();
-  alert("yaaa kamu pasti mau nyuri yaaa, kasihan tidak bisa🆘🥱🥱🥱🥱🥱🥱🆘 \n\n\n made by ♥️ ypicalsleepingboy");
+  alert("yaaa kamu pasti mau nyuri yaaa, kasihan tidak bisa🆘🥱🥱🥱🥱🥱🥱🆘 \n\n\n made by ♥️ Typicalsleepingboy");
 });
 
 document.addEventListener("keydown", function (e) {
   if (e.ctrlKey && e.shiftKey && (e.key === "I" || e.key === "i")) {
     e.preventDefault();
-    alert("yaaa kamu pasti mau nyuri yaaa, kasihan tidak bisa🆘🥱🥱🥱🥱🥱🥱🆘 \n\n\n made by ♥️ typicalsleepingboy");
+    alert("yaaa kamu pasti mau nyuri yaaa, kasihan tidak bisa🆘🥱🥱🥱🥱🥱🥱🆘 \n\n\n made by ♥️ Typicalsleepingboy");
   }
 });
