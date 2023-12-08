527c0531b3d6fcb6f6e3707c6aa6bfcb34905349
bolulembang <mpluto772@gmail.com>
2023-12-08 14:17:44 +0700

    delete page loader and add discord log for visiting website in typ js

--- script/typ.js
+++ script/typ.js
@@ -16,14 +16,54 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   });
+
+  // Your custom function to detect the user's location and device information
+  async function getUserInformation() {
+    const response = await fetch(
+      "https://api.ipregistry.co/?key=zqx0w4uhrljxqldg"
+    );
+    const data = await response.json();
+
+    return {
+      city: data.location.city,
+      device: data.device_type,
+      ip: data.ip,
+      time: new Date().toLocaleString(),
+    };
+  }
+
+  // Function to send data to Discord via webhook
+  async function sendToDiscord(data) {
+    const url =
+      "https://discord.com/api/webhooks/1182580231261921290/GQNh98TtfoIS16xTY-6MlhtMBGC7ubRyvqX_qP8_arP6Mr9u74xWwpD6zV_8f01IvbnV";
+    const message = `New user visit detected:\n\n- City: ${data.city}\n- Device: ${data.device}\n- IP: ${data.ip}\n- Time: ${data.time}`;
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ content: message }),
+    });
+
+    return response.ok;
+  }
+
+  // Call the function to get user information and send it to Discord
+  getUserInformation().then((data) => {
+    sendToDiscord(data);
+  });
 });
 
 window.addEventListener("load", () => {
-  const loader = document.querySelector(".loader");
+  const loader = document.getElementById("loader");
+  const body = document.body;
 
   loader.classList.add("loader--hidden");
 
   loader.addEventListener("transitionend", () => {
-    document.body.removeChild(loader);
+    // Remove the loading class to display the content
+    body.classList.remove("loading");
+    loader.parentNode.removeChild(loader);
   });
 });
